8306c0ca02c9d4bf4f7cf717f1d425236cba4d65
SENA <danilo15osorio@gmail.com>
2024-06-14 11:21:48 -0500

    Git phus

---
+++ ejercicios 3.kt
@@ -0,0 +1,95 @@
+fun main() {
+    val x = 3
+
+    when (x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+    }
+}
+//2 ejercicio//
+fun main() {
+    val x = 3
+
+    when (x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//3 ejercicio//
+fun main() {
+    val x = 3
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//4 ejercicio//
+fun main() {
+    val x = 3
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//5 ejercicio//
+fun main() {
+    val x = 3
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//6 ejercicio//
+fun main() {
+    val x = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//7 ejercicio//
+fun main() {
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+//8 ejercicio//
+fun main() {
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
+//9 ejercicio//
+fun main() {
+    val x: Any = 20
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
---
+++ src/main/kotlin/ejercicios 2.kt
@@ -0,0 +1,78 @@
+package org.example
+
+
+fun main() {
+    val Color = "Black"
+
+    if (Color == "Red") {
+        println("Stop")
+    } else if (Color == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
+}
+//2 jercisio//
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    }
+}
+//3 ejercicio//
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}
+//4 ejercicio//
+fun main() {
+    val trafficLightColor = "Yellow"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
+}
+//5 ejercicio//
+fun main() {
+    val trafficLightColor = "Green"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
+}
+//6 ejercicio//
+fun main() {
+    val trafficLightColor = "Green"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+
+    } else {
+        println("Go")
+    }
+}
+}
